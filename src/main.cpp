be71d795bacc40c8de9c04eaf32d559e8ccec62a
NeutrinoZh <neutrinozh@gmail.com>
2022-03-27 15:00:54 +0300

    Add GoodMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main(int countArg, char* args[]) {
+    system("read -N 1 -p 'Press any key to continue...'");
     std::cout << "Hello, World!" << std::endl;
     return EXIT_SUCCESS;
 }
